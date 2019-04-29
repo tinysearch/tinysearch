e4aeddd50c3501bd6e2153199503064304b19a37
Matthias Endler <matthias-endler@gmx.net>
2019-04-29 12:25:49 +0200

    Use a proper flag for enabling optimization

--- bin/src/main.rs
+++ bin/src/main.rs
@@ -35,7 +35,7 @@ struct Opt {
     index: PathBuf,
 
     /// Optimize the output using binaryen
-    #[structopt(name = "optimize", default_value = "false")]
+    #[structopt(short = "o", long = "optimize", default_value = "false")]
     optimize: bool,
 }
 
