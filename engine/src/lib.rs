60dc090561a6c4fd761904eae1922741b0ec44fe
Peter Trotman <petertrotman@gmail.com>
2019-10-24 10:52:42 +0100

    Filters no longer statically built into the binary, are dynamically loaded by javascript

--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -1,40 +1,61 @@
-#[macro_use]
-extern crate lazy_static;
-
+use js_sys::Uint8Array;
 use std::cmp::Reverse;
-use std::error::Error;
 use tinysearch_shared::{Filters, PostId, Score, Storage};
 use wasm_bindgen::prelude::*;
 
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-fn load_filters() -> Result<Filters, Box<Error>> {
-    let bytes = include_bytes!("../storage");
-    Ok(Storage::from_bytes(bytes)?.filters)
-}
-
-lazy_static! {
-    static ref FILTERS: Filters = load_filters().unwrap();
+#[wasm_bindgen]
+#[derive(Default)]
+pub struct TinySearch {
+    filters: Option<Filters>,
 }
 
 #[wasm_bindgen]
-pub fn search(query: String, num_results: usize) -> JsValue {
-    let search_terms: Vec<String> = query.split_whitespace().map(|s| s.to_lowercase()).collect();
+impl TinySearch {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Self {
+        TinySearch::default()
+    }
 
-    let mut matches: Vec<(&PostId, u32)> = FILTERS
-        .iter()
-        .map(|(name, filter)| (name, filter.score(&search_terms)))
-        .filter(|(_, score)| *score > 0)
-        .collect();
+    #[wasm_bindgen(js_name = "loadFilters")]
+    pub fn load_filters(&mut self, data: Uint8Array) -> Result<(), JsValue> {
+        let mut buffer: Vec<u8> = vec![0; data.byte_length() as usize];
+        data.copy_to(&mut buffer);
+        self.filters = Some(
+            Storage::from_bytes(&buffer)
+                .map_err(|e| e.to_string())?
+                .filters,
+        );
+        Ok(())
+    }
 
-    matches.sort_by_key(|k| Reverse(k.1));
+    #[wasm_bindgen]
+    pub fn search(&self, query: String, num_results: usize) -> JsValue {
+        if self.filters.is_none() {
+            return JsValue::from_serde(&Vec::<Vec<&PostId>>::new()).unwrap();
+        }
 
-    let results: Vec<&PostId> = matches
-        .iter()
-        .map(|(name, _)| name.to_owned())
-        .take(num_results)
-        .collect();
+        let filters = self.filters.as_ref().unwrap();
 
-    JsValue::from_serde(&results).unwrap()
+        let search_terms: Vec<String> =
+            query.split_whitespace().map(|s| s.to_lowercase()).collect();
+
+        let mut matches: Vec<(&PostId, u32)> = filters
+            .iter()
+            .map(|(name, filter)| (name, filter.score(&search_terms)))
+            .filter(|(_, score)| *score > 0)
+            .collect();
+
+        matches.sort_by_key(|k| Reverse(k.1));
+
+        let results: Vec<&PostId> = matches
+            .iter()
+            .map(|(name, _)| name.to_owned())
+            .take(num_results)
+            .collect();
+
+        JsValue::from_serde(&results).unwrap()
+    }
 }
