4df470dba558194e03850c3534e61bf291dbe61b
Matthias Endler <matthias-endler@gmx.net>
2020-02-26 01:22:01 +0100

    cleanup

--- pkg/tinysearch_engine_bg.d.ts
+++
@@ -1,5 +0,0 @@
-/* tslint:disable */
-export const memory: WebAssembly.Memory;
-export function search(a: number, b: number, c: number): number;
-export function __wbindgen_malloc(a: number): number;
-export function __wbindgen_realloc(a: number, b: number, c: number): number;
