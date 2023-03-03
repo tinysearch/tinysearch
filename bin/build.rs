69a27469ed0578946b11166bf0944ee752cea3fe
David Horner <dave@thehorners.com>
2023-03-03 18:52:22 -0500

    #165 Failing to find Cargo.toml in temp directory

--- bin/build.rs
+++ bin/build.rs
@@ -4,8 +4,8 @@ use includedir_codegen::Compression;
 
 fn main() {
     includedir_codegen::start("FILES")
-        .dir("../engine", Compression::Gzip)
-        .dir("../shared", Compression::Gzip)
+        .dir(concat!(env!("OUT_DIR"),"/engine"), Compression::Gzip)
+        .dir(concat!(env!("OUT_DIR"),"/shared"), Compression::Gzip)
         .build("engine.rs")
         .unwrap();
 }
