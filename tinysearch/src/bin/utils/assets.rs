773182ea859e509cff8bdd2f95eeaf25bb631eb6
Pavel Pletenev <cpp.create@gmail.com>
2023-03-10 23:41:22 +0300

    Fix CI
    Signed-off-by: Pavel Pletenev <cpp.create@gmail.com>

--- tinysearch/src/bin/utils/assets.rs
+++ tinysearch/src/bin/utils/assets.rs
@@ -1,7 +1,7 @@
-pub static CRATE_CARGO_TOML: &str = include_str!("../../../assets/crate/Cargo.toml");
-pub static CRATE_LIB_RS: &str = include_str!("../../../assets/crate/src/lib.rs");
+pub static CRATE_CARGO_TOML: &str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/assets/crate/Cargo.toml"));
+pub static CRATE_LIB_RS: &str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/assets/crate/src/lib.rs"));
 
 // Include a bare-bones HTML page template that demonstrates how tinysearch is used
-pub static DEMO_HTML: &str = include_str!("../../../assets/demo.html");
+pub static DEMO_HTML: &str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/assets/demo.html"));
 
-pub static STOP_WORDS: &str = include_str!("../../../assets/stopwords");
+pub static STOP_WORDS: &str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/assets/stopwords"));
